1b931b0e13724503e95086eac3311c58abc178cd
Craig Edwards <craig7edwards@gmail.com>
2020-10-13 15:49:04 -0400

    scroll

---
+++ main.js
@@ -0,0 +1,41 @@
+$(window).scroll(function() {
+  if (($(document).scrollTop() > 115) && ($(window).width() > 1399)) {
+    $('header').addClass('fixed');
+  }
+  else {
+    $('header').removeClass('fixed');
+  }
+});
+
+$("#navHam").click(function () {
+	$("header nav").toggleClass("open");
+	$("header .phoneContainer").toggleClass("open");
+});
+
+$("#navSearch").click(function () {
+	$(".searchContainer").addClass("open");
+});
+$(document).mouseup(function(e) 
+{
+	var containerS = $(".searchContainer");
+
+	// if the target of the click isn't the container nor a descendant of the container
+	if (!containerS.is(e.target) && containerS.has(e.target).length === 0) 
+	{
+	$(".searchContainer").removeClass("open");
+	}
+});
+
+$("#navCart").click(function () {
+	$(".cartContainer").addClass("open");
+});
+$(document).mouseup(function(e) 
+{
+	var containerC = $(".cartContainer");
+
+	// if the target of the click isn't the container nor a descendant of the container
+	if (!containerC.is(e.target) && containerC.has(e.target).length === 0) 
+	{
+	$(".cartContainer").removeClass("open");
+	}
+});
