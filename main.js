83fad8746691916c3f2620e4fb8082ec27d0fba6
Craig Edwards <craig7edwards@gmail.com>
2020-10-15 10:12:29 -0400

    smooth scrolling

--- main.js
+++ main.js
@@ -1,41 +1,59 @@
 $(window).scroll(function() {
-  if (($(document).scrollTop() > 20) && ($(window).width() > 1399)) {
+  if (($(document).scrollTop() > 1) && ($(window).width() > 1399)) {
     $('header').addClass('fixed');
   }
   else {
     $('header').removeClass('fixed');
+    $('header').removeClass('extend');
+		$("header nav").removeClass("open");
+		$("#navHam").removeClass("disable");
   }
 });
 
 $("#navHam").click(function () {
-	$("header nav").toggleClass("open");
-	$("header .phoneContainer").toggleClass("open");
+	$("header").addClass("extend");
+	$("header nav").addClass("open");
+	$("#navHam").addClass("disable");
+});
+$(document).mouseup(function(e) 
+{
+	var containerS = $("header nav");
+
+	// if the target of the click isn't the container nor a descendant of the container
+	if (!containerS.is(e.target) && containerS.has(e.target).length === 0) 
+	{
+		$("header").removeClass("extend");
+		$("header nav").removeClass("open");
+		$("#navHam").removeClass("disable");
+	}
 });
 
 $("#navSearch").click(function () {
 	$(".searchContainer").addClass("open");
+	$("#navSearch").addClass("disable");
 });
 $(document).mouseup(function(e) 
 {
 	var containerS = $(".searchContainer");
 
-	// if the target of the click isn't the container nor a descendant of the container
 	if (!containerS.is(e.target) && containerS.has(e.target).length === 0) 
 	{
-	$(".searchContainer").removeClass("open");
+		$(".searchContainer").removeClass("open");
+		$("#navSearch").removeClass("disable");
 	}
 });
 
 $("#navCart").click(function () {
 	$(".cartContainer").addClass("open");
+	$("#navCart").addClass("disable");
 });
 $(document).mouseup(function(e) 
 {
 	var containerC = $(".cartContainer");
 
-	// if the target of the click isn't the container nor a descendant of the container
 	if (!containerC.is(e.target) && containerC.has(e.target).length === 0) 
 	{
-	$(".cartContainer").removeClass("open");
+		$(".cartContainer").removeClass("open");
+		$("#navCart").removeClass("disable");
 	}
 });
