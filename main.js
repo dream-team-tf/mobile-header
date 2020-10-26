3d1b43d273c62982590baa596e6c71d07f09938a
SunfallCoast <34076815+SunfallCoast@users.noreply.github.com>
2020-10-26 13:03:24 -0400

    updated banners

--- main.js
+++ main.js
@@ -151,7 +151,7 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].className = slides[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].className += " active";
 }
